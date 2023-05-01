5d5091587059b5e2bd52a217597f221c2ef16693
Grayson Shogan <graysontoddshogan@gmail.com>
2023-05-01 14:47:03 -0700

    done

--- src/reducers/movieReducer.js
+++ src/reducers/movieReducer.js
@@ -1,21 +1,50 @@
-import { ADD_MOVIE, DELETE_MOVIE } from "../actions/movieActions.js";
+import {
+  ADD_MOVIE,
+  DELETE_MOVIE,
+  displayFavorites,
+  DISPLAY_FAVORITES,
+  ADD_FAVORITE,
+} from "../actions/movieActions.js";
 import movies from "./../data.js";
 
 const initialState = {
   movies: movies,
   appTitle: "IMDB Movie Database",
+  //   displayFavorites: false
 };
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case ADD_MOVIE:
-      return {
-        movies: [...state.movies, action.payload],
-      };
     case DELETE_MOVIE:
       return {
+        ...state,
         movies: state.movies.filter((item) => action.payload !== item.id),
       };
+    case ADD_MOVIE:
+      const newMovie = {
+        ...action.payload,
+        id: Date.now(),
+      };
+      return {
+        ...state,
+        movies: [...state.movies, newMovie],
+      };
+    case DISPLAY_FAVORITES:
+      return {
+        ...state,
+        displayFavorites: !state.displayFavorites,
+      };
+
+    case ADD_FAVORITE:
+      const newFav = {
+        ...action.payload,
+        id: Date.now(),
+      };
+      return {
+        ...state,
+        newFav,
+      };
+
     default:
       return state;
   }
